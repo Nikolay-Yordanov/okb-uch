f37bc8a3f6e241a6c95b046324675b0f64f8e6cc
Alexandre Levine <alexandrelevine@gmail.com>
2021-11-14 12:26:04 +0300

    Добавлены README Изменено many на N для множественных элементов

--- tools/xsd2xls.py
+++ tools/xsd2xls.py
@@ -15,12 +15,11 @@ def get_indent(depth):
 def get_traits(node):
     max_occurs = node.max_occurs
     if max_occurs == None:
-        max_occurs = 'many'
+        max_occurs = 'N'
     traits = str(node.min_occurs) + '..' + str(max_occurs)
     if node.parent.model == 'choice':
         traits += '/choice'
     return traits
-
 
 def process_elem(type, depth, limit, width):
     if depth == limit:
@@ -84,11 +83,11 @@ def write_sheet(title, schema, width, section, limit, *col_widths):
     process_elem(root_type, 0, limit, width)
 
 init_workbook()
-write_sheet("package", "UCH758Load.xsd", 3, 0, 2, 32, 100, 14)
-write_sheet("PersonEventType", "UCH758Load.xsd", 3, 1, 2, 32, 100, 14)
-write_sheet("OrgEventType", "UCH758Load.xsd", 3, 2, 2, 32, 100, 14)
-write_sheet("PersonCreditHistoryType", "UCH758Model.xsd", 4, 0, 5, 25, 40, 14, 80)
-write_sheet("OrgCreditHistoryType", "UCH758Model.xsd", 4, 1, 5, 25, 40, 14, 80)
-write_sheet("PersonReportType", "UCH758Report.xsd", 4, 0, 5, 25, 40, 14, 80)
-write_sheet("OrgReportType", "UCH758Report.xsd", 4, 1, 5, 25, 40, 14, 80)
+write_sheet("package", "UCH758Load.xsd", 3, 0, 2, 32, 100, 10)
+write_sheet("PersonEventType", "UCH758Load.xsd", 3, 1, 2, 32, 100, 10)
+write_sheet("OrgEventType", "UCH758Load.xsd", 3, 2, 2, 32, 100, 10)
+write_sheet("PersonCreditHistoryType", "UCH758Model.xsd", 4, 0, 5, 25, 40, 10, 80)
+write_sheet("OrgCreditHistoryType", "UCH758Model.xsd", 4, 1, 5, 25, 40, 10, 80)
+write_sheet("PersonReportType", "UCH758Report.xsd", 4, 0, 5, 25, 40, 10, 80)
+write_sheet("OrgReportType", "UCH758Report.xsd", 4, 1, 5, 25, 40, 10, 80)
 close_workbook()
