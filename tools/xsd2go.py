0dcbb9573b8269dea4e78c25eda1cdbdcc1f6ffe
Alexandre Levine <alexandrelevine@gmail.com>
2021-11-17 11:22:58 +0300

    XSD: Made base type BlockType for all blocks in Model
    Added infoDate attribute to this BlockType. As such all blocks would have possibility to associate infoDate with them.
    It'll be required for storing them with the date of info for each block per particular source
    
    Go:
    Added arg parsing to go generator.
    Added possibility to generate only root schema types.
    Added generation of attributes and omitempty to all xml tags

--- tools/xsd2go.py
+++ tools/xsd2go.py
@@ -1,15 +1,21 @@
 import xmlschema as xsd
+import argparse
 
 def capitalize(str):
     return str[:1].upper() + str[1:]
 
-def write_types(schema, go):
-    schema = xsd.XMLSchema(schema)
+def write_types(schema_name, go, skip_deps=False):
+    schema = xsd.XMLSchema(schema_name)
     f = open(go, "w")
     f.write("package main\n\n")
     for type in schema.complex_types:
+        if skip_deps and type.schema.name != schema_name:
+            continue
         f.write("// " + type.annotation.documentation[0].text + "\n")
         f.write("type Xs" + type.local_name + " struct {\n")
+        for attr in type.attributes:
+            row = "  " + capitalize(attr) + " *string " + '`xml:"' + attr + ',attr,omitempty"`'
+            f.write(row + "\n")
         for elem in type.content.iter_elements():
             if elem.max_occurs == 0:
                 continue
@@ -22,9 +28,19 @@ def write_types(schema, go):
                 row += "Xs" + elem.type.local_name
             else:
                 row += "string"
-            row += ' `xml:"' + elem.local_name + '"`'
+            row += ' `xml:"' + elem.local_name + ',omitempty"`'
             f.write(row + "\n")
         f.write("}\n\n")
     f.close()
 
-write_types("UCH758Load.xsd", "uch.go")
+ap = argparse.ArgumentParser()
+
+ap.add_argument("-x", "--xsd", required=False,
+   help="XSD Schema to parse", default="UCH758Load.xsd")
+ap.add_argument("-o", "--output", required=False,
+   help="Filename to write result into", default="uch.go")
+ap.add_argument("-s", "--shallow", required=False,
+   help="Parse root schema only", action="store_true", default=False)
+args = vars(ap.parse_args())
+
+write_types(args['xsd'], args['output'], args['shallow'])
